63cbf2df30b8ed255f65e7ceeee8eec858e4097c
Guosheng Wang <gw2389@adcu.columbia.edu>
2019-06-23 01:24:59 -0400

    first

---
+++ CosmosGetStarted.py
@@ -0,0 +1,27 @@
+import azure.cosmos.documents as documents
+import azure.cosmos.cosmos_client as cosmos_client
+import azure.cosmos.errors as errors
+import datetime
+
+import samples.Shared.config as cfg
+
+config = {
+    'ENDPOINT': 'https://001007d1-0ee0-4-231-b9ee.documents.azure.com:443/',
+    'PRIMARYKEY': 'TwTqcL2eQb21pelkAZacrfoqTvtBSvivaZgz3sH7AIze3ivpqxpNOJsM0mfdTb3p8cIhnkiObdqzwrECJLj2bQ==',
+    'DATABASE': 'db1',
+    'CONTAINER': 'employee'
+}
+
+
+database_link = 'dbs/db1'
+collection_link = database_link + '/colls/employee'
+
+client = cosmos_client.CosmosClient(url_connection=config['ENDPOINT'], auth={
+                                    'masterKey': config['PRIMARYKEY']})
+
+documentlist = list(client.ReadItems(collection_link, {'maxItemCount':10}))
+        
+print('Found {0} documents'.format(documentlist.__len__()))
+        
+for doc in documentlist:
+    print('Document Id: {0}'.format(doc.get('id')))
---
+++ CosmosGetStarted1.py
@@ -0,0 +1,20 @@
+from pydocumentdb import document_client
+
+uri = 'https://001007d1-0ee0-4-231-b9ee.documents.azure.com:443/'
+key = 'TwTqcL2eQb21pelkAZacrfoqTvtBSvivaZgz3sH7AIze3ivpqxpNOJsM0mfdTb3p8cIhnkiObdqzwrECJLj2bQ=='
+
+client = document_client.DocumentClient(uri, {'masterKey': key})
+
+db_id = 'db1'
+db_query = "select * from r where r.id = '{0}'".format(db_id)
+db = list(client.QueryDatabases(db_query))[0]
+db_link = db['_self']
+
+coll_id = 'employee'
+coll_query = "select * from r where r.id = '{0}'".format(coll_id)
+coll = list(client.QueryCollections(db_link, coll_query))[0]
+coll_link = coll['_self']
+
+docs = client.ReadDocuments(coll_link)
+print(list(docs))
+
